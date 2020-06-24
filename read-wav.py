27397262b2b0b1ae0e848635c21ba84fe3ca0736
Carol Xu <carolx715@gmail.com>
2020-06-24 19:42:08 -0400

    preliminary code to create spectrogram image out of .wav file

--- read-wav.py
+++ read-wav.py
@@ -24,6 +24,47 @@ Length in Seconds: {length_in_seconds}'''.format(
             first_sample=first_sample,
             second_sample=second_sample,
             length_in_seconds=length_in_sec))
-        return
+        return wave_file
 
-parse_wave("wav/major/scale_c_major.wav")
+wf = parse_wave("wav/major/scale_c_major.wav")
+print(wf)
+
+import librosa
+import librosa.display
+from path import Path
+
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import models
+from tensorflow.python.keras.layers.advanced_activations import LeakyReLU
+from tensorflow.python.keras.optimizers import Adam
+# import tensorflow.python.keras.backend as K
+import pylab
+import matplotlib.pyplot as plt
+from matplotlib import figure
+import gc
+
+from memory_profiler import memory_usage
+import os
+import pandas as pd
+from glob import glob
+import numpy as np
+
+def create_spectrogram(filename,name):
+    plt.interactive(False)
+    clip, sample_rate = librosa.load(filename, sr=None)
+    fig = plt.figure(figsize=[0.72,0.72])
+    ax = fig.add_subplot(111)
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.set_frame_on(False)
+    S = librosa.feature.melspectrogram(y=clip, sr=sample_rate)
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
+    filename = name + '.jpg'
+    plt.savefig(filename, dpi=400, bbox_inches='tight',pad_inches=0)
+    plt.close()
+    fig.clf()
+    plt.close(fig)
+    plt.close('all')
+    del filename,name,clip,sample_rate,fig,ax,S
+
+create_spectrogram("wav/major/scale_c_major.wav", "C Major Scale")
