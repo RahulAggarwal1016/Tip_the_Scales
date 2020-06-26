b0a740c009c296208ca17e829955b8317e349dfb
Pop0097 <dhruvrawat09@gmail.com>
2020-06-26 12:39:55 -0400

    Model now predicts

--- html/js/predict.js
+++ html/js/predict.js
@@ -1,21 +1,49 @@
 var url = "https://teachablemachine.withgoogle.com/models/un_XbaxjZ/";
 
-
-
-(async () => {
+$(async function() {
+    //loads model
     model = await tmImage.load(url + "model.json", url + "metadata.json")
-    console.log("Model is loaded")
+    console.log("Model is loaded") 
 
     maxPredictions = model.getTotalClasses();
-    console.log(maxPredictions);
 
     $('#test-button').click(async function() {
+        //Uses the model to predict what the scale is
         var prediction = await model.predict(document.getElementById("selected-image"));
-        console.log(prediction);
+
+                        //Harmonic Minor               //Major                  //Melodic Minor             //Natural Minor             //Other
+        var values = [prediction[0].probability, prediction[1].probability, prediction[2].probability, prediction[3].probability, prediction[4].probability];
+        var maxProb = Math.max.apply(Math, values);
+        console.log(maxProb);
+
+        if(maxProb == prediction[0].probability && prediction[0].probability >= 0.40) {
+            console.log("It is Harmonic Minor");
+            print_results("Harmonic Minor");
+        } else if(maxProb == prediction[1].probability && prediction[1].probability >= 0.40) {
+            console.log("It is Major");
+            print_results("Major");
+        } else if(maxProb == prediction[2].probability && prediction[2].probability >= 0.40) {
+            console.log("It is Melodic Minor");
+            print_results("Melodic Minor");
+        } else if(maxProb == prediction[3].probability && prediction[3].probability >= 0.40) {
+            console.log("It is Natural Minor");
+            print_results("Natural Minor");
+        } else {
+            console.log("It is OTHEREERER");
+            print_results("Other");
+        }
     })
 });
 
-$.ajax({
+
+function print_results(result) {
+    $('#result-text').html(result);
+}
+
+
+/*
+$("#target").click(function(){
+  $.ajax({
     type: "POST",
     url: "../python/convert-to-jpg.py",
     data: { param: "filename.wav"}
@@ -23,6 +51,5 @@ $.ajax({
      // print path to img
      console.log("pathtoimg");
   });
-
-
-
+});
+*/
