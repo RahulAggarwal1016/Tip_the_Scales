1144abd223e62833cd8a19ee4251014a2be9373a
Pop0097 <dhruvrawat09@gmail.com>
2020-06-26 13:04:40 -0400

    Co-authored-by: Carol Xu <carolx715@gmail.com>

--- html/js/predict.js
+++ html/js/predict.js
@@ -7,37 +7,38 @@ $(async function() {
 
     maxPredictions = model.getTotalClasses();
 
-    $('#test-button').click(async function() {
-        //Uses the model to predict what the scale is
-        var prediction = await model.predict(document.getElementById("selected-image"));
+    //Uses the model to predict what the scale is
+    document.getElementById("input-image").src = "./img/image.jpg";                             /* Rahul, you will need an <img> HTML tag to hold this image which is in the html/img directory. */
+    var prediction = await model.predict(document.getElementById("input-image"));               /* Rahul, calls image that you saved in the <img> tag */
+    console.log(prediction);
 
-                        //Harmonic Minor               //Major                  //Melodic Minor             //Natural Minor             //Other
-        var values = [prediction[0].probability, prediction[1].probability, prediction[2].probability, prediction[3].probability, prediction[4].probability];
-        var maxProb = Math.max.apply(Math, values);
-        console.log(maxProb);
+                    //Harmonic Minor               //Major                  //Melodic Minor             //Natural Minor             //Other
+    var values = [prediction[0].probability, prediction[1].probability, prediction[2].probability, prediction[3].probability, prediction[4].probability];
+    var maxProb = Math.max.apply(Math, values); //Finds which element has the highest value
+    console.log(maxProb);
 
-        if(maxProb == prediction[0].probability && prediction[0].probability >= 0.40) {
-            console.log("It is Harmonic Minor");
-            print_results("Harmonic Minor");
-        } else if(maxProb == prediction[1].probability && prediction[1].probability >= 0.40) {
-            console.log("It is Major");
-            print_results("Major");
-        } else if(maxProb == prediction[2].probability && prediction[2].probability >= 0.40) {
-            console.log("It is Melodic Minor");
-            print_results("Melodic Minor");
-        } else if(maxProb == prediction[3].probability && prediction[3].probability >= 0.40) {
-            console.log("It is Natural Minor");
-            print_results("Natural Minor");
-        } else {
-            console.log("It is OTHEREERER");
-            print_results("Other");
-        }
-    })
+    //Determines which 
+    if(maxProb == prediction[0].probability && prediction[0].probability >= 0.40) {
+        console.log("It is Harmonic Minor");
+        print_results("Harmonic Minor");
+    } else if(maxProb == prediction[1].probability && prediction[1].probability >= 0.40) {
+        console.log("It is Major");
+        print_results("Major");
+    } else if(maxProb == prediction[2].probability && prediction[2].probability >= 0.40) {
+        console.log("It is Melodic Minor");
+        print_results("Melodic Minor");
+    } else if(maxProb == prediction[3].probability && prediction[3].probability >= 0.40) {
+        console.log("It is Natural Minor");
+        print_results("Natural Minor");
+    } else {
+        console.log("It is OTHEREERER");
+        print_results("Other");
+    }
 });
 
 
 function print_results(result) {
-    $('#result-text').html(result);
+    //$('#result-text').html(result);                                                           /* Rahul, this prints the results on a <p> HTML tag. */
 }
 
 
